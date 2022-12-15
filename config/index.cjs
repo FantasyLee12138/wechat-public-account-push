1d8acd33c06a5620b121cc2b69b6b158f7c440d4
FantasyLee12138 <120617072+FantasyLee12138@users.noreply.github.com>
2022-12-15 12:14:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oL8oI6LFACAG8eYzWgurYuYLNcxc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hXHOjBR6_w-DE1_XwW3e1MSwEeEdJ3vgyYzVoU5DMIE',
+      useTemplateId: '8hpTc5Y5duy6mGGXtf5lfWnAy9qTHU5tKAfoADqoVv0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-21',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'hXHOjBR6_w-DE1_XwW3e1MSwEeEdJ3vgyYzVoU5DMIE',
+  CALLBACK_TEMPLATE_ID: '8hpTc5Y5duy6mGGXtf5lfWnAy9qTHU5tKAfoADqoVv0',
 
   CALLBACK_USERS: [
     {
